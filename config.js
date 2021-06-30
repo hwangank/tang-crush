2035dde880f13bb87c6b16f229d9edde020c1913
hwangank <86708173+hwangank@users.noreply.github.com>
2021-06-30 14:29:14 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'Xem Thêm Về Hoàng',
     title: 'Phải chăng em đã yêu Hoàng ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy của Hoàng ',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy Hoàng ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Hoàng à :<',
     question:'Trên thế giới hơn 7 tỉ người mà Hoàng lại là duy nhất, người em nên yêu 😘  <3',
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Biết ngay mà, Anh yêu Em <3.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' // https://m.me/3000.TRU.2480
+    messLink: 'http://fb.com//m.me/3000.TRU.2480
 }
